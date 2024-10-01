3e99dd672b994ff22bef311cc45913387cf219ab
Carol Wargo <Carolwargo@gmail.com>
2024-10-01 17:11:43 -0400

    fixed login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,83 +1,46 @@
 import React from 'react';
+import {useContext, useState} from "react";
+import {Navigate} from "react-router-dom";
+import {UserContext} from "../UserContext";
+
 import {
   MDBBtn,
   MDBContainer,
   MDBRow,
   MDBCol,
   MDBInput,
-    MDBCheckbox,
-    MDBIcon,
-    MDBCard,
-    MDBCardBody
 }
 from 'mdb-react-ui-kit';
 
 function LoginPage() {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [redirect,setRedirect] = useState(false);
+  const {setUserInfo} = useContext(UserContext);
+  async function login(ev) {
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({email, password}),
+      headers: {'Content-Type':'application/json'},
+      credentials: 'include',
+    });
+    if (response.ok) {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
+        setRedirect(true);
+      });
+    } else {
+      alert('wrong credentials');
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
   return (
     <div>
-    <MDBContainer fluid className='my-5'>
-
-    <MDBRow className='g-0 align-items-center'>
-      <MDBCol col='6'>
-
-        <MDBCard className='my-5 cascading-right' style={{background: 'hsla(0, 0%, 100%, 0.55)',  backdropFilter: 'blur(30px)'}}>
-          <MDBCardBody className='p-5 shadow-5 text-center'>
-
-            <h2 className="fw-bold mb-5">Sign up now</h2>
-
-            <MDBRow>
-              <MDBCol col='6'>
-                <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
-              </MDBCol>
-
-              <MDBCol col='6'>
-                <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text'/>
-              </MDBCol>
-            </MDBRow>
-
-            <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email'/>
-            <MDBInput wrapperClass='mb-4' label='Password' id='form4' type='password'/>
-
-            <div className='d-flex justify-content-center mb-4'>
-              <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
-            </div>
-
-            <MDBBtn className='w-100 mb-4' size='md'>sign up</MDBBtn>
-
-            <div className="text-center">
-
-              <p>or sign up with:</p>
-
-              <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                <MDBIcon fab icon='facebook-f' size="sm"/>
-              </MDBBtn>
-
-              <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                <MDBIcon fab icon='twitter' size="sm"/>
-              </MDBBtn>
-
-              <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                <MDBIcon fab icon='google' size="sm"/>
-              </MDBBtn>
-
-              <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                <MDBIcon fab icon='github' size="sm"/>
-              </MDBBtn>
-
-            </div>
-
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-
-      <MDBCol col='6'>
-        <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" class="w-100 rounded-4 shadow-4"
-          alt="" fluid/>
-      </MDBCol>
-
-    </MDBRow>
-
-  </MDBContainer>
+   
     <MDBContainer className="my-5 gradient-form">
 
       <MDBRow>
@@ -93,15 +56,29 @@ function LoginPage() {
 
             <p>Please login to your account</p>
 
-
-            <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
-            <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
-
+            <form className="login" onSubmit={login}>
+            <MDBInput 
+            wrapperClass='mb-4' 
+            placeholder="Email@mail.com"
+            value={email}
+            onChange={ev => setEmail(ev.target.value)}
+            type='email'
+            />
+       
+            <MDBInput 
+            wrapperClass='mb-4' 
+            id='form2' 
+            type='password'
+            placeholder="Password"
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}
+            />
 
             <div className="text-center pt-1 mb-5 pb-1">
               <MDBBtn className="mb-4 w-100 gradient-custom-2">Sign in</MDBBtn>
               <a className="text-muted" href="#!">Forgot password?</a>
             </div>
+            </form>
 
             <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
               <p className="mb-0">Don't have an account?</p>
@@ -117,7 +94,7 @@ function LoginPage() {
         <MDBCol col='6' className="mb-5">
           <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
 
-            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+            <div className="text-black px-3 py-4 p-md-5 mx-md-4">
               <h4 class="mb-4">We are more than just a company</h4>
               <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
