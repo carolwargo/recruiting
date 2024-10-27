2a9e504eea8a2bdea782d3abef74c8257415150f
Carol Wargo <Carolwargo@gmail.com>
2024-10-27 12:23:50 -0400

    update

--- api/index.js
+++ api/index.js
@@ -17,3 +17,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+/**MONGODB_URI=mongodb+srv://carolwargo:8DyaILn1LnL4Ttql@cluster0.jplhrrc.mongodb.net/recruit?retryWrites=true&w=majority&appName=Cluster0
+JWT_SECRET=1HERE|2!S*3@|4SECRET*5KEYno
+ */
