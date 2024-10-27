a8d50e9c6a113d19514e2aefa32f0c6fa102f658
Carol Wargo <Carolwargo@gmail.com>
2024-10-27 17:38:18 -0400

    updae

--- api/index.js
+++ api/index.js
@@ -1,19 +1,14 @@
+require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const authRoute = require("./Routes/AuthRoute");
-const { MONGO_URL, PORT } = process.env;
+const connectDB = require('./config/connection');
 
-mongoose
-  .connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB is  connected successfully"))
-  .catch((err) => console.error(err));
+connectDB();  
+
+const PORT = process.env.PORT || 4000;  
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
@@ -26,6 +21,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 
 app.use(express.json());
--- api/routes/AuthRoute.js
+++ api/routes/AuthRoute.js
@@ -1,6 +1,7 @@
-const { Signup } = require("../controllers/AuthController");
-const router = require("express").Router();
+const { Signup, Login } = require('../Controllers/AuthController')
+const router = require('express').Router()
 
-router.post("/signup", Signup);
+router.post('/signup', Signup)
+router.post('/login', Login)
 
-module.exports = router;
+module.exports = router
